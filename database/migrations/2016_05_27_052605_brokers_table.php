3fa7bf01d2838edded7f91de52038f8f3473e9a0
kenjos75 <kenjos75@yahoo.com>
2016-05-27 01:35:16 -0400

    First upload of davao houses projects file

---
+++ database/migrations/2016_05_27_052605_brokers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BrokersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brokers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('middle_name');
+            $table->string('profile_picture_thumbnail');
+            $table->string('profile_picture_original');
+            $table->string('additional_information');
+            $table->string('contact_number');
+            $table->string('email_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
